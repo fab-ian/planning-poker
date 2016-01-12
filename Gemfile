6a9c0a0f32e1df715173b344eabcd57284c690e8
Fabian K <grafska@gmail.com>
2016-01-12 11:35:23 +0100

    Add gems: devise, rails_admin

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source 'https://rubygems.org'
 
 gem 'haml'
 gem 'haml-rails'
+gem 'devise'
+gem 'rails_admin'
+
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'user/index'
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
+  #get 'user/index'
 
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
