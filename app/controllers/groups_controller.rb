e06c1b4182d65549838b8d78f1c2d513e72c205b
Fabian K <grafska@gmail.com>
2016-03-20 19:27:06 +0100

    Remove 'grou'p resources

--- app/controllers/groups_controller.rb
+++
@@ -1,7 +0,0 @@
-class GroupsController < ApplicationController
-  def new
-  end
-
-  def edit
-  end
-end
--- app/models/group.rb
+++
@@ -1,4 +0,0 @@
-class Group < ActiveRecord::Base
-  belongs_to :user
-  has_many :group_users
-end
