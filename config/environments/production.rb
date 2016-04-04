37d21d9232df674b1c7a2826e20a438ae1b0dd61
Fabian <info@znajdzbieg.pl>
2016-04-04 23:46:43 +0200

    Set 'sendmail'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,12 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.raise_delivery_errors = true  
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.default_options = {from: 'info@biegokracja.pl'}
+
 
 end
 
-# Rails.application.routes.default_url_options[:host] = 'https://fk-planning-poker.herokuapp.com'
+ Rails.application.routes.default_url_options[:host] = 'https://fk-planning-poker.herokuapp.com'
