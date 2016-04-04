45a0828aa35338e9e0e00ee9b102afc549be75fd
Fabian K <grafska@gmail.com>
2016-04-04 17:58:26 +0200

    Change email settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   
+  config.action_mailer.raise_delivery_errors = true  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
@@ -48,7 +49,6 @@ Rails.application.configure do
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true  }
-
 end
 
 Rails.application.routes.default_url_options[:host] = 'localhost:3000'
