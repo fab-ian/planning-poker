6d6657168c5ad8fa241ff5c3c0dea11e41a6e6d7
Fabian K <grafska@gmail.com>
2016-04-07 22:07:48 +0200

    Fix 'disable' help generator

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,9 @@ module PlanningPoker
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-    generate.helper false
+    
+    config.generators do |generate|
+      generate.helper false
+    end
   end
 end
